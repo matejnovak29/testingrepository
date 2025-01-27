98ba9f1b70d65a0af8176a7584622e4be74d93c6
Matěj Novák <78020157+matejnovak29@users.noreply.github.com>
2025-01-27 13:29:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "dbtcloud" {
-  token  = var.dbt_cloud_api_token
+  api_token  = var.dbt_cloud_api_token
   account_id = var.dbt_cloud_account_id
   host_url   = "https://cloud.getdbt.com/api"
 }
@@ -18,13 +18,30 @@ resource "dbtcloud_project" "example_project" {
   description = "Project created via Terraform"
 }
 
-resource "dbtcloud_job" "example_job" {
-  name        = "Example Job"
+resource "dbtcloud_environment" "example_environment" {
+  name        = "Production"
+  type        = "deployment" # Can also be "development" based on your need
   project_id  = dbtcloud_project.example_project.id
+  dbt_version = "1.4.0"
+}
+
+resource "dbtcloud_job" "example_job" {
+  name         = "Example Job"
+  project_id   = dbtcloud_project.example_project.id
+  environment_id = dbtcloud_environment.example_environment.id # Link to the environment
+  dbt_version  = "1.4.0"
   execute_steps = [
     "dbt run",
     "dbt test"
   ]
+
+  # Schedule trigger
+  triggers {
+    schedule {
+      cron      = "0 12 * * *" # Daily at 12:00 UTC
+      timezone  = "UTC"
+    }
+  }
 }
 
 output "dbt_cloud_job_id" {
